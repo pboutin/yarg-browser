348a410934efafda7a89712f8aa94251916a897b
Pascal Boutin <pboutin@medyx.com>
2026-08-27 16:47:27 -0400

    Fix scores charts responsiveness

--- src/screens/song/scores-charts.tsx
+++ src/screens/song/scores-charts.tsx
@@ -172,11 +172,11 @@ const ScoresCharts = ({ scores }: Props) => {
   }, [scores]);
 
   return (
-    <div className="flex-1 flex flex-col gap-8 p-4">
-      <div className="relative flex-1">
+    <div className="flex-1 min-w-0 min-h-0 flex flex-col gap-8 p-4">
+      <div className="relative flex-1 min-h-0 min-w-0">
         <canvas ref={scoreCanvasRef} />
       </div>
-      <div className="relative flex-1">
+      <div className="relative flex-1 min-h-0 min-w-0">
         <canvas ref={percentCanvasRef} />
       </div>
     </div>
